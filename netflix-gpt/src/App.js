4bfe8c371c2636a554a91a462cbf314d8bf21616
AbhiRockz123 <abhishekparmar988@gmail.com>
2024-03-30 12:17:30 +0530

    Initial Commit 1st

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2 className="text-center">Netflix GPT</h2>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
